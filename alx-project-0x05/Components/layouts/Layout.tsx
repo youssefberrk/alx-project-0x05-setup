090044477204a9d51524ca5ae4d6c11425d437b6
youssef <yberrakouan.lol@gmail.com>
2025-01-14 12:36:43 +0000

    Include a ContextAPI for global state management

--- alx-project-0x05/Components/layouts/Layout.tsx
+++ alx-project-0x05/Components/layouts/Layout.tsx
@@ -1,4 +1,3 @@
-import { LayoutProps } from "@/interface";
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+import Layout from "@/Components/layouts/Layout";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import { CountProvider } from "@/context/ CountContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
+  );
 }
