6b7deadb76aa1c82fdcb31a6e548df055274ce4e
youssef <yberrakouan.lol@gmail.com>
2025-01-14 12:20:01 +0000

    updates

---
+++ alx-project-0x05/Components/layouts/Layout.tsx
@@ -0,0 +1,20 @@
+import { LayoutProps } from "@/interface";
+import { ReactNode } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
